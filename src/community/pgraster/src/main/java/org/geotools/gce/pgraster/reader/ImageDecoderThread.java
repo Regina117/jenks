camptocamp/geoserver

pgraster: introduce pgraster community module after deletion from upstream
import geotools imagemosaic jdbc code base and remove
everything unrelated to pgraster.

src/community/pgraster/src/main/java/org/geotools/gce/pgraster/reader/ImageDecoderThread.java
@@ -1,0 +1,175 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.gce.pgraster.reader;
+
+import com.sun.media.jai.codec.ByteArraySeekableStream;
+import com.sun.media.jai.codec.ImageCodec;
+import com.sun.media.jai.codec.ImageDecoder;
+import com.sun.media.jai.codec.SeekableStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.media.jai.PlanarImage;
+import org.geotools.geometry.GeneralEnvelope;
+import org.geotools.util.logging.Logging;
+
+/**
+ * This class is used for multithreaded decoding of the tiles read from the database.
+ *
+ * <p>Decoding errors result in a log message and the result is <code>null</code>.
+ *
+ * <p>Creating an ImageDocoderThread object with a null or empty bytearray result in a <code>null
+ * </code> value for <code>g {@link #getBufferedImage()}</code>
+ *
+ * @author mcr, christian
+ */
+class ImageDecoderThread extends Thread {
+
+    protected static final Logger LOGGER = Logging.getLogger(ImageDecoderThread.class);
+
+    LinkedBlockingQueue<TileQueueElement> tileQueue;
+
+    GeneralEnvelope requestEnvelope;
+
+    ImageLevelInfo levelInfo;
+
+    private byte[] imageBytes;
+
+    private String location;
+
+    private GeneralEnvelope dbTileEnvelope;
+
+    /**
+     * Decoder thread.
+     *
+     * @param bytes the image bytes
+     * @param location the tile name
+     * @param dbTileEnvelope the georeferencing information for the tile
+     * @param requestEnvelope the requested envelope
+     * @param levelInfo the proper levelInfo
+     * @param tileQueue the queue where to put the result
+     */
+    public ImageDecoderThread(
+            byte[] bytes,
+            String location,
+            GeneralEnvelope dbTileEnvelope,
+            GeneralEnvelope requestEnvelope,
+            ImageLevelInfo levelInfo,
+            LinkedBlockingQueue<TileQueueElement> tileQueue) {
+        this.tileQueue = tileQueue;
+        this.requestEnvelope = requestEnvelope;
+        this.levelInfo = levelInfo;
+        this.imageBytes = bytes; // maybe it would be better to save a copy
+        this.location = location;
+        this.dbTileEnvelope = dbTileEnvelope;
+    }
+
+    @Override
+    public void run() {
+        if ((imageBytes == null) || (imageBytes.length == 0)) { // nothing to do
+            return;
+        }
+
+        try {
+
+            BufferedImage dbTileImage = null;
+
+            boolean triedFromStream = false;
+            if (levelInfo.getCanImageIOReadFromInputStream()) {
+                dbTileImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
+                triedFromStream = true;
+            }
+            if (dbTileImage == null) {
+                if (triedFromStream) {
+                    LOGGER.warning("Could not read " + location + " from stream, switch to JAI");
+                }
+                dbTileImage = readImage2(imageBytes);
+            }
+
+            if (requestEnvelope.contains(dbTileEnvelope, true)) {
+                tileQueue.add(new TileQueueElement(location, dbTileImage, dbTileEnvelope));
+            } else {
+                GeneralEnvelope intersectionEnvelope = new GeneralEnvelope(dbTileEnvelope);
+                intersectionEnvelope.intersect(requestEnvelope);
+
+                // x and y refers here to Image coordinates (so y is inverted wrt Envelope)
+                int xmin =
+                        (int)
+                                (Math.round(
+                                        (intersectionEnvelope.getMinimum(0)
+                                                        - dbTileEnvelope.getMinimum(0))
+                                                / levelInfo.getResX()));
+                int ymin =
+                        (int)
+                                (Math.round(
+                                        (dbTileEnvelope.getMaximum(1)
+                                                        - intersectionEnvelope.getMaximum(1))
+                                                / levelInfo.getResY()));
+                int xmax =
+                        (int)
+                                (Math.round(
+                                        (intersectionEnvelope.getMaximum(0)
+                                                        - dbTileEnvelope.getMinimum(0))
+                                                / levelInfo.getResX()));
+                int ymax =
+                        (int)
+                                (Math.round(
+                                        (dbTileEnvelope.getMaximum(1)
+                                                        - intersectionEnvelope.getMinimum(1))
+                                                / levelInfo.getResY()));
+                int width = xmax - xmin;
+                int height = ymax - ymin;
+
+                if ((width > 0) && (height > 0)) {
+
+                    BufferedImage clippedImage = dbTileImage.getSubimage(xmin, ymin, width, height);
+
+                    tileQueue.add(
+                            new TileQueueElement(location, clippedImage, intersectionEnvelope));
+                }
+            }
+        } catch (IOException ex) {
+            LOGGER.severe("Decorde error for tile " + location);
+            LOGGER.log(Level.SEVERE, ex.getLocalizedMessage(), ex);
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Fallback Method, in some jre implementations, ImageIO.read(InputStream in) returns null. If
+     * this happens, this method is called, which is not so efficient but it works
+     */
+    private BufferedImage readImage2(byte[] imageBytes) throws IOException {
+        try (SeekableStream stream = new ByteArraySeekableStream(imageBytes)) {
+            String decoderName = null;
+
+            for (String dn : ImageCodec.getDecoderNames(stream)) {
+                decoderName = dn;
+                break;
+            }
+
+            ImageDecoder decoder = ImageCodec.createImageDecoder(decoderName, stream, null);
+            PlanarImage img = PlanarImage.wrapRenderedImage(decoder.decodeAsRenderedImage());
+            return img.getAsBufferedImage();
+        }
+    }
+}

src/community/pgraster/src/main/java/org/geotools/gce/pgraster/reader/PostgisRasterReaderState.java
@@ -1,0 +1,145 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.gce.pgraster.reader;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.LinkedBlockingQueue;
+import javax.media.jai.RenderedImageAdapter;
+import org.geotools.gce.pgraster.PostgisRasterGridCoverage2DReader;
+import org.geotools.geometry.GeneralEnvelope;
+import org.geotools.image.util.ImageUtilities;
+
+/**
+ * Holds the state of the {@link PostgisRasterGridCoverage2DReader} making the reader thread safe.
+ *
+ * @author mcr
+ * @since 2.6
+ */
+public class PostgisRasterReaderState {
+    public static int DEFAULT_IMAGE_TYPE = BufferedImage.TYPE_3BYTE_BGR;
+
+    private Color backgroundColor = null;
+
+    private Color outputTransparentColor = null;
+
+    private Rectangle renderedImageRectangle = null;
+
+    private boolean xAxisSwitch = false;
+
+    private GeneralEnvelope requestedEnvelope = null;
+
+    private GeneralEnvelope requestedEnvelopeTransformed = null;
+
+    private GeneralEnvelope requestedEnvelopeTransformedExpanded = null;
+
+    private ImageLevelInfo imageLevelInfo = null;
+
+    private final LinkedBlockingQueue<TileQueueElement> tileQueue = new LinkedBlockingQueue<>();
+
+    /** @return BufferdImage filled with outputTransparentColor */
+    public BufferedImage getEmptyImage(int width, int height) {
+        Color backGroundcolor = getBackgroundColor();
+        Color outputTransparentColor = getOutputTransparentColor();
+        BufferedImage emptyImage =
+                new BufferedImage(width, height, PostgisRasterReaderState.DEFAULT_IMAGE_TYPE);
+        Graphics2D g2D = (Graphics2D) emptyImage.getGraphics();
+        Color save = g2D.getColor();
+        g2D.setColor(backGroundcolor);
+        g2D.fillRect(0, 0, emptyImage.getWidth(), emptyImage.getHeight());
+        g2D.setColor(save);
+        if (outputTransparentColor != null) {
+            emptyImage =
+                    new RenderedImageAdapter(
+                                    ImageUtilities.maskColor(outputTransparentColor, emptyImage))
+                            .getAsBufferedImage();
+        }
+        return emptyImage;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public Color getOutputTransparentColor() {
+        return outputTransparentColor;
+    }
+
+    public void setOutputTransparentColor(Color outputTransparentColor) {
+        this.outputTransparentColor = outputTransparentColor;
+    }
+
+    public Rectangle getRenderedImageRectangle() {
+        return renderedImageRectangle;
+    }
+
+    public void setRenderedImageRectangle(Rectangle renderedImageRectangle) {
+        this.renderedImageRectangle = renderedImageRectangle;
+    }
+
+    public boolean isXAxisSwitch() {
+        return xAxisSwitch;
+    }
+
+    public void setXAxisSwitch(boolean axisSwitch) {
+        xAxisSwitch = axisSwitch;
+    }
+
+    public GeneralEnvelope getRequestedEnvelope() {
+        return requestedEnvelope;
+    }
+
+    public void setRequestedEnvelope(GeneralEnvelope requestedEnvelope) {
+        this.requestedEnvelope = requestedEnvelope;
+    }
+
+    public GeneralEnvelope getRequestedEnvelopeTransformed() {
+        return requestedEnvelopeTransformed;
+    }
+
+    public void setRequestedEnvelopeTransformed(GeneralEnvelope requestedEnvelopeTransformed) {
+        this.requestedEnvelopeTransformed = requestedEnvelopeTransformed;
+    }
+
+    public GeneralEnvelope getRequestedEnvelopeTransformedExpanded() {
+        return requestedEnvelopeTransformedExpanded;
+    }
+
+    public void setRequestedEnvelopeTransformedExpanded(
+            GeneralEnvelope requestedEnvelopeTransformedExpanded) {
+        this.requestedEnvelopeTransformedExpanded = requestedEnvelopeTransformedExpanded;
+    }
+
+    public ImageLevelInfo getImageLevelInfo() {
+        return imageLevelInfo;
+    }
+
+    public void setImageLevelInfo(ImageLevelInfo imageLevelInfo) {
+        this.imageLevelInfo = imageLevelInfo;
+    }
+
+    public LinkedBlockingQueue<TileQueueElement> getTileQueue() {
+        return tileQueue;
+    }
+}

src/community/pgraster/src/main/java/org/geotools/gce/pgraster/reader/TileQueueElement.java
@@ -1,0 +1,62 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.gce.pgraster.reader;
+
+import java.awt.image.BufferedImage;
+import org.geotools.geometry.GeneralEnvelope;
+
+/**
+ * Class for holding tile info for composing the resulting image. Objects of this class will be put
+ * in the queue from ImageDecoderThread and read from ImageComposerThread
+ *
+ * @author mcr
+ */
+class TileQueueElement {
+
+    public static TileQueueElement ENDELEMENT = new TileQueueElement(null, null, null);
+
+    /** name of the tile */
+    private String name;
+
+    /** the BufferedImage */
+    private BufferedImage tileImage;
+
+    /** The georeferencing information */
+    private GeneralEnvelope envelope;
+
+    public GeneralEnvelope getEnvelope() {
+        return envelope;
+    }
+
+    public TileQueueElement(String name, BufferedImage tileImage, GeneralEnvelope envelope) {
+        this.name = name;
+        this.tileImage = tileImage;
+        this.envelope = envelope;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BufferedImage getTileImage() {
+        return tileImage;
+    }
+
+    public boolean isEndElement() {
+        return getName() == null && getTileImage() == null && getEnvelope() == null;
+    }
+}

Regina117/jenks

src/community/pgraster/src/main/java/org/geotools/gce/pgraster/reader/ImageDecoderThread.java
@@ -1,0 +1,175 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.gce.pgraster.reader;
+
+import com.sun.media.jai.codec.ByteArraySeekableStream;
+import com.sun.media.jai.codec.ImageCodec;
+import com.sun.media.jai.codec.ImageDecoder;
+import com.sun.media.jai.codec.SeekableStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.media.jai.PlanarImage;
+import org.geotools.geometry.GeneralBounds;
+import org.geotools.util.logging.Logging;
+
+/**
+ * This class is used for multithreaded decoding of the tiles read from the database.
+ *
+ * <p>Decoding errors result in a log message and the result is <code>null</code>.
+ *
+ * <p>Creating an ImageDocoderThread object with a null or empty bytearray result in a <code>null
+ * </code> value for <code>g {@link #getBufferedImage()}</code>
+ *
+ * @author mcr, christian
+ */
+class ImageDecoderThread extends Thread {
+
+    protected static final Logger LOGGER = Logging.getLogger(ImageDecoderThread.class);
+
+    LinkedBlockingQueue<TileQueueElement> tileQueue;
+
+    GeneralBounds requestEnvelope;
+
+    ImageLevelInfo levelInfo;
+
+    private byte[] imageBytes;
+
+    private String location;
+
+    private GeneralBounds dbTileEnvelope;
+
+    /**
+     * Decoder thread.
+     *
+     * @param bytes the image bytes
+     * @param location the tile name
+     * @param dbTileEnvelope the georeferencing information for the tile
+     * @param requestEnvelope the requested envelope
+     * @param levelInfo the proper levelInfo
+     * @param tileQueue the queue where to put the result
+     */
+    public ImageDecoderThread(
+            byte[] bytes,
+            String location,
+            GeneralBounds dbTileEnvelope,
+            GeneralBounds requestEnvelope,
+            ImageLevelInfo levelInfo,
+            LinkedBlockingQueue<TileQueueElement> tileQueue) {
+        this.tileQueue = tileQueue;
+        this.requestEnvelope = requestEnvelope;
+        this.levelInfo = levelInfo;
+        this.imageBytes = bytes; // maybe it would be better to save a copy
+        this.location = location;
+        this.dbTileEnvelope = dbTileEnvelope;
+    }
+
+    @Override
+    public void run() {
+        if ((imageBytes == null) || (imageBytes.length == 0)) { // nothing to do
+            return;
+        }
+
+        try {
+
+            BufferedImage dbTileImage = null;
+
+            boolean triedFromStream = false;
+            if (levelInfo.getCanImageIOReadFromInputStream()) {
+                dbTileImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
+                triedFromStream = true;
+            }
+            if (dbTileImage == null) {
+                if (triedFromStream) {
+                    LOGGER.warning("Could not read " + location + " from stream, switch to JAI");
+                }
+                dbTileImage = readImage2(imageBytes);
+            }
+
+            if (requestEnvelope.contains(dbTileEnvelope, true)) {
+                tileQueue.add(new TileQueueElement(location, dbTileImage, dbTileEnvelope));
+            } else {
+                GeneralBounds intersectionEnvelope = new GeneralBounds(dbTileEnvelope);
+                intersectionEnvelope.intersect(requestEnvelope);
+
+                // x and y refers here to Image coordinates (so y is inverted wrt Envelope)
+                int xmin =
+                        (int)
+                                (Math.round(
+                                        (intersectionEnvelope.getMinimum(0)
+                                                        - dbTileEnvelope.getMinimum(0))
+                                                / levelInfo.getResX()));
+                int ymin =
+                        (int)
+                                (Math.round(
+                                        (dbTileEnvelope.getMaximum(1)
+                                                        - intersectionEnvelope.getMaximum(1))
+                                                / levelInfo.getResY()));
+                int xmax =
+                        (int)
+                                (Math.round(
+                                        (intersectionEnvelope.getMaximum(0)
+                                                        - dbTileEnvelope.getMinimum(0))
+                                                / levelInfo.getResX()));
+                int ymax =
+                        (int)
+                                (Math.round(
+                                        (dbTileEnvelope.getMaximum(1)
+                                                        - intersectionEnvelope.getMinimum(1))
+                                                / levelInfo.getResY()));
+                int width = xmax - xmin;
+                int height = ymax - ymin;
+
+                if ((width > 0) && (height > 0)) {
+
+                    BufferedImage clippedImage = dbTileImage.getSubimage(xmin, ymin, width, height);
+
+                    tileQueue.add(
+                            new TileQueueElement(location, clippedImage, intersectionEnvelope));
+                }
+            }
+        } catch (IOException ex) {
+            LOGGER.severe("Decorde error for tile " + location);
+            LOGGER.log(Level.SEVERE, ex.getLocalizedMessage(), ex);
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Fallback Method, in some jre implementations, ImageIO.read(InputStream in) returns null. If
+     * this happens, this method is called, which is not so efficient but it works
+     */
+    private BufferedImage readImage2(byte[] imageBytes) throws IOException {
+        try (SeekableStream stream = new ByteArraySeekableStream(imageBytes)) {
+            String decoderName = null;
+
+            for (String dn : ImageCodec.getDecoderNames(stream)) {
+                decoderName = dn;
+                break;
+            }
+
+            ImageDecoder decoder = ImageCodec.createImageDecoder(decoderName, stream, null);
+            PlanarImage img = PlanarImage.wrapRenderedImage(decoder.decodeAsRenderedImage());
+            return img.getAsBufferedImage();
+        }
+    }
+}

src/community/pgraster/src/main/java/org/geotools/gce/pgraster/reader/PostgisRasterReaderState.java
@@ -1,0 +1,145 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.gce.pgraster.reader;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.LinkedBlockingQueue;
+import javax.media.jai.RenderedImageAdapter;
+import org.geotools.gce.pgraster.PostgisRasterGridCoverage2DReader;
+import org.geotools.geometry.GeneralBounds;
+import org.geotools.image.util.ImageUtilities;
+
+/**
+ * Holds the state of the {@link PostgisRasterGridCoverage2DReader} making the reader thread safe.
+ *
+ * @author mcr
+ * @since 2.6
+ */
+public class PostgisRasterReaderState {
+    public static int DEFAULT_IMAGE_TYPE = BufferedImage.TYPE_3BYTE_BGR;
+
+    private Color backgroundColor = null;
+
+    private Color outputTransparentColor = null;
+
+    private Rectangle renderedImageRectangle = null;
+
+    private boolean xAxisSwitch = false;
+
+    private GeneralBounds requestedEnvelope = null;
+
+    private GeneralBounds requestedEnvelopeTransformed = null;
+
+    private GeneralBounds requestedEnvelopeTransformedExpanded = null;
+
+    private ImageLevelInfo imageLevelInfo = null;
+
+    private final LinkedBlockingQueue<TileQueueElement> tileQueue = new LinkedBlockingQueue<>();
+
+    /** @return BufferdImage filled with outputTransparentColor */
+    public BufferedImage getEmptyImage(int width, int height) {
+        Color backGroundcolor = getBackgroundColor();
+        Color outputTransparentColor = getOutputTransparentColor();
+        BufferedImage emptyImage =
+                new BufferedImage(width, height, PostgisRasterReaderState.DEFAULT_IMAGE_TYPE);
+        Graphics2D g2D = (Graphics2D) emptyImage.getGraphics();
+        Color save = g2D.getColor();
+        g2D.setColor(backGroundcolor);
+        g2D.fillRect(0, 0, emptyImage.getWidth(), emptyImage.getHeight());
+        g2D.setColor(save);
+        if (outputTransparentColor != null) {
+            emptyImage =
+                    new RenderedImageAdapter(
+                                    ImageUtilities.maskColor(outputTransparentColor, emptyImage))
+                            .getAsBufferedImage();
+        }
+        return emptyImage;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public Color getOutputTransparentColor() {
+        return outputTransparentColor;
+    }
+
+    public void setOutputTransparentColor(Color outputTransparentColor) {
+        this.outputTransparentColor = outputTransparentColor;
+    }
+
+    public Rectangle getRenderedImageRectangle() {
+        return renderedImageRectangle;
+    }
+
+    public void setRenderedImageRectangle(Rectangle renderedImageRectangle) {
+        this.renderedImageRectangle = renderedImageRectangle;
+    }
+
+    public boolean isXAxisSwitch() {
+        return xAxisSwitch;
+    }
+
+    public void setXAxisSwitch(boolean axisSwitch) {
+        xAxisSwitch = axisSwitch;
+    }
+
+    public GeneralBounds getRequestedEnvelope() {
+        return requestedEnvelope;
+    }
+
+    public void setRequestedEnvelope(GeneralBounds requestedEnvelope) {
+        this.requestedEnvelope = requestedEnvelope;
+    }
+
+    public GeneralBounds getRequestedEnvelopeTransformed() {
+        return requestedEnvelopeTransformed;
+    }
+
+    public void setRequestedEnvelopeTransformed(GeneralBounds requestedEnvelopeTransformed) {
+        this.requestedEnvelopeTransformed = requestedEnvelopeTransformed;
+    }
+
+    public GeneralBounds getRequestedEnvelopeTransformedExpanded() {
+        return requestedEnvelopeTransformedExpanded;
+    }
+
+    public void setRequestedEnvelopeTransformedExpanded(
+            GeneralBounds requestedEnvelopeTransformedExpanded) {
+        this.requestedEnvelopeTransformedExpanded = requestedEnvelopeTransformedExpanded;
+    }
+
+    public ImageLevelInfo getImageLevelInfo() {
+        return imageLevelInfo;
+    }
+
+    public void setImageLevelInfo(ImageLevelInfo imageLevelInfo) {
+        this.imageLevelInfo = imageLevelInfo;
+    }
+
+    public LinkedBlockingQueue<TileQueueElement> getTileQueue() {
+        return tileQueue;
+    }
+}

src/community/pgraster/src/main/java/org/geotools/gce/pgraster/reader/TileQueueElement.java
@@ -1,0 +1,62 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.gce.pgraster.reader;
+
+import java.awt.image.BufferedImage;
+import org.geotools.geometry.GeneralBounds;
+
+/**
+ * Class for holding tile info for composing the resulting image. Objects of this class will be put
+ * in the queue from ImageDecoderThread and read from ImageComposerThread
+ *
+ * @author mcr
+ */
+class TileQueueElement {
+
+    public static TileQueueElement ENDELEMENT = new TileQueueElement(null, null, null);
+
+    /** name of the tile */
+    private String name;
+
+    /** the BufferedImage */
+    private BufferedImage tileImage;
+
+    /** The georeferencing information */
+    private GeneralBounds envelope;
+
+    public GeneralBounds getEnvelope() {
+        return envelope;
+    }
+
+    public TileQueueElement(String name, BufferedImage tileImage, GeneralBounds envelope) {
+        this.name = name;
+        this.tileImage = tileImage;
+        this.envelope = envelope;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BufferedImage getTileImage() {
+        return tileImage;
+    }
+
+    public boolean isEndElement() {
+        return getName() == null && getTileImage() == null && getEnvelope() == null;
+    }
+}