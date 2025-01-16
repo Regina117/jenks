936db6eb7fb690a3453f7cc5511af8af20c139dc
Regina117 <r.kunafina@gmail.com>
2025-01-16 12:52:15 +0300

    update name

--- addvm.tf
+++ addvm.tf
@@ -33,7 +33,7 @@ resource "yandex_compute_instance" "master" {
   }
 
   metadata = {
-    ssh-keys= "regina:${file("~/.ssh/id_ed25519.pub")}"
+    ssh-keys= "regina:${file("~/.ssh/id_rsa.pub")}"
     user-data = <<-EOT
       #cloud-config
       runcmd:
