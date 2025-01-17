08fcc8cda1a8d9c57a9bdd6903bdf0c133dc8a16
Regina117 <r.kunafina@gmail.com>
2025-01-17 16:46:22 +0300

    add pipeline

--- addvm.tf
+++ addvm.tf
@@ -51,41 +51,6 @@ resource "yandex_compute_instance" "jenkins" {
   }
 }
 
-resource "yandex_compute_instance" "build" {
-  name        = "build"
-  platform_id = "standard-v2"
-  resources {
-    cores  = 4
-    memory = 4
-  }
-  boot_disk {
-    initialize_params {
-      image_id = "fd8421uec874p44sq4j3"
-      size     = 35
-    }
-  }
-  network_interface {
-    subnet_id = var.subnet_id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys= "regina:${file("/home/regina/.ssh/id_rsa.pub")}"
-    user-data = <<-EOT
-      #cloud-config
-      users:
-      - name: regina
-        sudo: ALL=(ALL) NOPASSWD:ALL
-        groups: sudo
-        ssh_authorized_keys:
-          - ${file("/home/regina/.ssh/id_rsa.pub")}
-      runcmd:
-        - apt update && apt upgrade -y
-        - apt install -y openjdk-11-jdk maven
-    EOT
-  }
-}
-
 resource "yandex_compute_instance" "prod" {
   name        = "prod"
   platform_id = "standard-v2"
@@ -164,10 +129,6 @@ variable "subnet_id" {}
 
 output "jenkins_ip" {
   value = yandex_compute_instance.jenkins.network_interface[0].nat_ip_address
-}
-
-output "build_ip" {
-  value = yandex_compute_instance.build.network_interface[0].nat_ip_address
 }
 
 output "prod_ip" {
